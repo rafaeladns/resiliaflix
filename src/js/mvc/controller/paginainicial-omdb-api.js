30cb741b59cfea14fe94a8bc9ca1467d22fb73c1
Rafaela Dias <93568221+rafaeladns@users.noreply.github.com>
2022-01-04 15:33:17 -0300

    Alterações js

--- src/js/mvc/controller/paginainicial-omdb-api.js
+++ src/js/mvc/controller/paginainicial-omdb-api.js
@@ -1,10 +1,33 @@
 //Vai controlar o input e chamar a API quando o filme for digitado//
-let filme = ''
-
-let botao = document.getElementById('botaoPesquisar')
-botao.addEventListener('click' ,()=>{
-    filme = $('input-pesquisa').val();
+// let botao = document.getElementById('botaoPesquisar')
+// botao.addEventListener('click' ,()=>{
+//    let filme = $('input-pesquisa').val();
     
-    apiFilme(filme)
+//     apiFilme(filme)
   
+// })
+
+function apiFilme(nomeFilme){
+    if(nomeFilme ){
+        const url = `https://www.omdbapi.com/?apikey=284df65c&t=${nomeFilme}`
+        const options = {
+            method: 'GET'
+        }
+    }
+}
+
+$('#botaoPesquisar').on('click', async function(filme){
+    filme = document.getElementById('input-pesquisa').value
+    let url =  `https://www.omdbapi.com/?apikey=284df65c&t=${filme}`
+    let apiResposta = await fetch(url).then(response => response.json());
+   console.log(apiResposta)
+   try{
+       if(apiResposta.Title !== undefined){
+        resultadoApi(apiResposta)
+       } else {
+           throw new Error ('Filme não encontrado. Tente novamente.')
+       }
+    }catch(error){
+        apiErro(error)
+   }
 })
